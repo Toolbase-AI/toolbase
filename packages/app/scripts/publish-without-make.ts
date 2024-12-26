11ef84327b15152ef5423ab8643019e6585b9c9b
Gavin Ching <gavinchingy@gmail.com>
2024-12-26 15:19:44 -0800

    Sign installer with a dry run, add in windows icon

--- packages/app/scripts/publish-without-make.ts
+++ packages/app/scripts/publish-without-make.ts
@@ -1,6 +1,15 @@
+import process from 'node:process';
 import { api as forge } from '@electron-forge/core';
 
+console.log(process.argv[2]);
+
+const isDryRun = process.argv[2] === '--dry-run';
+
+console.log(isDryRun ? 'Starting a dry run' : 'Publishing from dry run');
+
 await forge.publish({
+  dryRun: isDryRun,
+  dryRunResume: !isDryRun,
   makeOptions: {
     skipPackage: true,
   },
