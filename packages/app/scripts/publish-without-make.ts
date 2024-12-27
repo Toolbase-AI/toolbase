7023e21c576ddd153ced54920d0726bdf5599c56
Gavin <gavinchingy@gmail.com>
2024-12-26 16:25:43 -0800

    Bump to 0.0.2 - Finalize Windows (#3)
    * Sign installer with a dry run, add in windows icon
    
    * Quick debugging
    
    * Debugging again for PowerShell
    
    * Include both indexes for dry run arg
    
    * Remove console log
    
    * Bump to 0.02

--- packages/app/scripts/publish-without-make.ts
+++ packages/app/scripts/publish-without-make.ts
@@ -1,6 +1,14 @@
+import process from 'node:process';
 import { api as forge } from '@electron-forge/core';
 
+const isDryRun =
+  process.argv[2] === '--dry-run' || process.argv[3] === '--dry-run';
+
+console.log(isDryRun ? 'Starting a dry run' : 'Publishing from dry run');
+
 await forge.publish({
+  dryRun: isDryRun,
+  dryRunResume: !isDryRun,
   makeOptions: {
     skipPackage: true,
   },
